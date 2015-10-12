f6436abb5680a0871cb2cd978550391495112991
furious-programming <furiousprogramming@gmail.com>
2015-10-12 19:47:55 +0200

    RemoveAllLinks - used new field and new procedures/functions

--- Source/TSInfoFiles.pp
+++ Source/TSInfoFiles.pp
@@ -3115,21 +3115,18 @@ begin
 end;
 
 
-procedure TTSInfoTree.RemoveAllLinks(const ANodePath: UTF8String = '');
+procedure TTSInfoTree.RemoveAllLinks(const ANodePath: UTF8String);
 var
   nodeParent: TTSInfoNode;
 begin
-  if FReadOnlyMode then
-    ThrowException(EM_READ_ONLY_MODE_VIOLATION, [])
+  if FReadOnly then
+    ThrowException(EM_READ_ONLY_MODE_VIOLATION)
   else
   begin
-    if IsCurrentNodeSymbol(ANodePath) then
+    if IsCurrentNodePath(ANodePath) then
       nodeParent := FCurrentNode
     else
-    begin
-      IncludeTrailingIdentsDelimiter(ANodePath);
-      nodeParent := FindNode(ANodePath, False);
-    end;
+      nodeParent := FindNode(IncludeTrailingIdentsDelimiter(ANodePath), False);
 
     if nodeParent <> nil then
     begin
