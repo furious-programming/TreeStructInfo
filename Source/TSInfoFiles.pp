259afe40c233646163a78092a207760ca2362972
furious programming <furiousprogramming@gmail.com>
2017-03-29 00:19:27 +0200

    notation changed

--- Source/TSInfoFiles.pp
+++ Source/TSInfoFiles.pp
@@ -1899,14 +1899,14 @@ end;
 
 function TSimpleTSInfoTree.ReadCurrency(const AAttrPath: String; ADefault: Currency): Currency;
 var
-  attrRead: TTSInfoAttribute;
+  LAttribute: TTSInfoAttribute;
 begin
-  attrRead := FindAttribute(ExcludeTrailingIdentsDelimiter(AAttrPath), False);
+  LAttribute := FindAttribute(ExcludeTrailingIdentsDelimiter(AAttrPath), False);
 
-  if attrRead = nil then
+  if LAttribute = nil then
     Result := ADefault
   else
-    Result := TTSInfoDataConverter.ValueToCurrency(attrRead.Value, DefaultFormatSettings, ADefault);
+    Result := TTSInfoDataConverter.ValueToCurrency(LAttribute.Value, DefaultFormatSettings, ADefault);
 end;
 
 
